5097a4cf92f990e2cc1a2f91cb9d5387f8079a14
LuisCldrn <Luis.cldrn@yahoo.com>
2025-02-22 15:04:02 -0600

    updated Resume

--- src/app/aboutme/aboutme.component.ts
+++ src/app/aboutme/aboutme.component.ts
@@ -7,6 +7,6 @@ import { Component } from '@angular/core';
 })
 export class AboutmeComponent {
 
-  aboutmetext: string = "Fully committed to the philosophy of life-long learning, I'm a full stack developer with a deep passion for JavaScript, React and all things web development. The unique combination of creativity, logic, technology and never running out of new things to discover, drives my excitement and passion for web development. When I'm not at my computer I like to spend my time reading, keeping fit and playing guitar."
+  aboutmetext: string = "Fully committed to the philosophy of life-long learning, I'm a full stack developer with a deep passion for JavaScript, Angular and all things Software Development. The unique combination of creativity, logic, technology and never running out of new things to discover, drives my excitement and passion for Software Developing. When I'm not at my computer I like to spend my time drawing, painting and gaming."
 
 }
